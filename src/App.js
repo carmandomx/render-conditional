c46386e7042dffe8ea9f91872a98eb017f1aa4b7
Carlos Reyes <carmandoreyes93@gmail.com>
2021-05-11 20:20:29 -0500

    Ejemplos de Renderizado Condicional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header ({ text }) {
+  return <h3>{text}</h3>
 }
 
-export default App;
+function Container ({ toggle, handleToggle, text }) {
+  return (
+    <header className='App-header'>
+      {toggle && <Header text={text} />}
+
+      <button
+        onClick={() => {
+          handleToggle(!toggle)
+        }}
+      >
+        {toggle ? 'Ocultar' : 'Mostrar'}
+      </button>
+    </header>
+  )
+}
+
+function App () {
+  const [toggle, setToggle] = useState(true)
+  const [data, setData] = useState(null)
+  const [dataTwo, setDataTwo] = useState(null)
+  const [hasData, setHasData] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({ text: 'Esto funciona' })
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataTwo({ age: 27 })
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    if (data && dataTwo) {
+      setHasData(true)
+    }
+  }, [data, dataTwo])
+
+  return (
+    <div className='App'>
+      {hasData && (
+        <Container text={data.text} toggle={toggle} handleToggle={setToggle} />
+      )}
+    </div>
+  )
+}
+
+export default App
